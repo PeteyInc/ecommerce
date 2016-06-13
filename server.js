b452bc43b4875dec9f9499e1e73eb32b2f58554f
Jared M. Peterson <jared@peteyinc.com>
2016-06-13 16:29:26 -0600

    api calls done

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var corsOptions = {
 
 var db = mongojs('ecommerce');
 var prodColl = db.collection('products');
+var ObjectId = mongojs.ObjectId;
 
 app.use(bodyParser.json());
 app.use(express.static('./public'));
@@ -18,12 +19,57 @@ app.use(cors(corsOptions));
 // app.use(express.static(__dirname + './public'));
 
 
-// `POST /api/products`
-// `GET /api/products`
-// `GET /api/products/:id`
-// `PUT /api/products/:id`
-// `DELETE /api/products/:id`
+app.post('/api/products', function(req, res, next) {
+  prodColl.save(req.body, function(err, response) {
+    if (err) return res.status(500).send(err);
+    res.send(response);
+  });
+});
+app.get('/api/products', function(req, res, next) {
+  prodColl.find(req.query, function (err, response) {
+    if (!err) res.status(200).json(response);
+  });
+});
 
+
+app.get('/api/products/:id', function(req, res, next) {
+  prodColl.findOne({_id: ObjectId(req.params.id)}, function (err, response) {
+    if (!err) {
+      console.log(ObjectId());
+      res.status(200).json(response);
+    }
+  });
+});
+
+app.put('/api/products/:id', function(req, res, next) {
+  if (!req.params.id) {
+    return res.status(400).json('id query needed');
+  }
+  prodColl.update({_id: ObjectId(req.params.id)}, req.body, function(err, response) {
+    if (err) {
+      return res.status(500).json(err);
+    } else {
+      return res.json(response);
+    }
+  });
+});
+
+app.delete('/api/products/:id', function(req, res, next) {
+  if (!req.params.id) {
+    return res.status(400).json('id query needed');
+  }
+  prodColl.remove({_id: ObjectId(req.params.id)}, function(err, response) {
+    if (err) {
+      return res.status(500).json(err);
+    } else {
+      return res.json(response);
+    }
+  });
+});
+
+db.runCommand({ping: 1}, function (err, res) {
+    if(!err && res.ok) console.log('products live');
+});
 app.listen(port, function() {
   console.log("Listening to port", port);
 });
