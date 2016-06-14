33ce978123c0de36c0f98226da9f4c5d5c3f1492
Jared M. Peterson <jared@peteyinc.com>
2016-06-14 14:13:51 -0600

    remove mongojs add mongoose

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var order = require('gulp-order');
+var ngAnnotate = require('gulp-ng-annotate');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('es6', function() {
+  gulp.src(['public/js/*.js'])
+  .pipe(babel({
+    'presets': ['es2015']
+  }))
+  .pipe(order(['public/js/*.js']))
+  .pipe(concat('main.js'))
+  .pipe(gulp.dest('public/dist/'));
+});
+
+gulp.task('sass', function() {
+  return gulp.src('public/styles/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('public/dist/'));
+});
+
+gulp.watch('public/styles/*.scss', ['sass']);
+gulp.watch(['public/js/*.js'], ['es6']);
+
+gulp.task('default', ['es6', 'sass']);
